a2ee4080a6fb4150ac3d1ee64a008f95e19958eb
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-08-01 20:43:30 +0600

    first commit

---
+++ A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+    return 0;
+}
